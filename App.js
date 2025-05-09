e52e308818e0195fd69d430623b7dbbdf0b164fc
naveenkasukurthi <127668472+naveenkasukurthi@users.noreply.github.com>
2025-05-09 15:10:02 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ModernCalendar from './components/ModernCalendar';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ModernCalendar />
+    </div>
+  );
+}
+
+export default App;
